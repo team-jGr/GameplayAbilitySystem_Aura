c72308aeb4a870766bb95d6346b3a20a61bc4464
Jeremy and Raebecca Powell <jeremydpowell@live.com>
2023-10-20 15:53:58 -0500

    9. Enhanced Input

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,33 @@
+// Copyright Jeremy and Raebecca Powell
+
+
+#include "Player/AuraPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/LocalPlayer.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
