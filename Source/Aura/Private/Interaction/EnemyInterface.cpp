8d25707f8d41753a5f6d22c80b23c83cfee4ffd6
Jeremy and Raebecca Powell <jeremydpowell@live.com>
2023-10-22 13:10:29 -0500

    13. Enemy Interface

---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright Jeremy and Raebecca Powell
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
