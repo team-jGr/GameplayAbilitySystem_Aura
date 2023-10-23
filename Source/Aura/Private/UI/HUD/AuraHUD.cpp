cf850e3b66d4690d80ec80935c930f7dec6c878d
Jeremy and Raebecca Powell <jeremydpowell@live.com>
2023-10-23 14:04:01 -0500

    32. Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// Copyright Jeremy and Raebecca Powell
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
