cc683fc15abee85b0cde471eee4c1a710e826418
Jeremy and Raebecca Powell <jeremydpowell@live.com>
2023-11-02 13:01:16 -0500

    86. Gameplay Tags Singleton

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,13 @@
+// Copyright Jeremy and Raebecca Powell
+
+
+#include "AuraGameplayTags.h"
+
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduces damage taken. Improves block chance."));
+}
