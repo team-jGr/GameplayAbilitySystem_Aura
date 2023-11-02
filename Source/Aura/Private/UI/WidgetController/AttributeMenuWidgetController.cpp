7f37f3b63a31249989d635a3f4b2d0df51e8c982
Jeremy and Raebecca Powell <jeremydpowell@live.com>
2023-11-02 14:54:50 -0500

    90. Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Jeremy and Raebecca Powell
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
