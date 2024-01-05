7112843e3f6492bb950ee89c7fcf87a619d96d2f
Jeremy and Raebecca Powell <jeremydpowell@live.com>
2024-01-04 22:01:09 -0600

    154. Aura Ability System Globals

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+// Copyright Jeremy and Raebecca Powell
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
